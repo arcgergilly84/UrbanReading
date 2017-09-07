9caf647139c2c272506e943a1a2eac454f38754f
David <archergilly84@gmail.com>
2017-09-07 20:40:02 +0100

    tidied up code with Main.java Start() method.

--- src/UrbanReading/Main.java
+++ src/UrbanReading/Main.java
@@ -14,12 +14,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("Views/Main.fxml"));
         primaryStage.setTitle("Book Collection");
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
+        primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
+
     }
 
     public static void main(String[] args) { launch(args); }
